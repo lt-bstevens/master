014d3da8a9651d42df0929da762142137a9540f8
bstevens <bstevens@lifetouch.com>
2017-11-30 07:45:52 -0600

    adding built folder

---
+++ built/driverProviders/index.d.ts
@@ -0,0 +1,13 @@
+export * from './attachSession';
+export * from './browserStack';
+export * from './direct';
+export * from './driverProvider';
+export * from './hosted';
+export * from './local';
+export * from './mock';
+export * from './sauce';
+export * from './testObject';
+import { DriverProvider } from './driverProvider';
+import { Config } from '../config';
+export declare let buildDriverProvider: (config: Config) => DriverProvider;
+export declare let logWarnings: (providerType: string, config: Config) => void;
